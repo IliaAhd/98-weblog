a124a2d0bab53686611ecc1af20dfe2964ded006
Ilia <iliya.ahadi@gmail.com>
2025-09-13 13:54:08 +0330

    Improved styles

--- src/app/profile/components/Profile/Profile.tsx
+++ src/app/profile/components/Profile/Profile.tsx
@@ -12,25 +12,38 @@ export default function Profile({
   isPrivateProfile?: boolean;
 }) {
   return (
-    <div className={`title-bar inactive ${styles.profile}`}>
-      <div className={styles.title}>
-        <Image
-          className={styles.avatar}
-          src={avatar || user.image || "/default-avatar.png"}
-          alt={`${user?.name}'s avatar`}
-          width={48}
-          height={48}
-        />
-        <div className={`title-bar-text ${styles.info}`}>
-          <h1 style={{ fontSize: "1rem" }}>{user?.name}</h1>
-          <div className={styles.status}>
-            <span style={{ fontWeight: "bold" }}>{user.Post?.length}</span>
-            <span>Posts</span>
+    <div className={`window ${styles.width}`}>
+      <div className="window-body">
+        <div className={styles.profile}>
+          <Image
+            className={styles.avatar}
+            src={avatar || user.image || "/default-avatar.png"}
+            alt={`${user?.name}'s avatar`}
+            width={67}
+            height={67}
+          />
+
+          <div className={`title-bar-text ${styles.info}`}>
+            <div className={styles.items}>
+              <div style={{ fontSize: "1.5rem" }}>{user?.name}</div>
+
+              <div className={`${styles.status} ${styles.mHidden}`}>
+                <span>{user.Post?.length}</span>
+                <span>Posts</span>
+              </div>
+            </div>
+
+            {isPrivateProfile && (
+              <div style={{ fontSize: "1rem" }}>{user?.email}</div>
+            )}
+
+            <div className={`${styles.status} ${styles.dHidden}`}>
+              <span>{user.Post?.length}</span>
+              <span>Posts</span>
+            </div>
           </div>
         </div>
       </div>
-
-      {isPrivateProfile && <h2 style={{ fontSize: "1rem" }}>{user?.email}</h2>}
     </div>
   );
 }
